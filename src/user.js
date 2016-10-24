c41570831ae8727e93bab1775e758abbd93796d2
Elanmarkowitz <elanmarkowitz@ucla.edu>
2016-10-24 01:17:34 -0700

    improved user model

--- src/user.js
+++ src/user.js
@@ -2,9 +2,21 @@ Sequelize.define('user', {
   firstName: {
     type: Sequelize.STRING,
     allowNull: false,
+    validate: {
+      is: ["^[a-z]+$",'i']
+    }
   },
   lastName: {
     type: Sequelize.STRING,
     allowNull: false,
+    validate: {
+      is: ["^[a-z]+$",'i']
+    }
   },
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+      isEmail: true
+    }
+  }
 })
