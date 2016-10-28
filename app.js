5d43828edcbc73d1ef13e8b71a3c16b95618cacd
Elanmarkowitz <elanmarkowitz@ucla.edu>
2016-10-28 13:41:15 -0700

    pre heroku setup

--- app.js
+++ app.js
@@ -3,16 +3,26 @@ var db = require('./platform/db');
 var mw = require('./lib/middlewares');
 var co = require('co');
 
+const router = new mw.router();
+
 var app = koa();
 //app.use(mw.router());
+app.use(mw.bodyParser());
+
+router.post('/users', function *(next){
+  this.body = "hi user";
+  console.log(next.request.body);
+});
+
+app.use(router.routes());
 
 app.use(function *(){
   var url = this.request.url;
   this.body = "hello world";
 });
 
-app.listen(3000);
-
 co(function *(){
   var connection = null;
 })
+
+app.listen(3000);
