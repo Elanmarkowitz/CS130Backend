07ddae348379e272f6bf1560a28b3dacf1cb8102
pointatnick <nicholashma@gmail.com>
2016-10-29 14:43:11 -0700

    changed GET routing

--- app.js
+++ app.js
@@ -4,30 +4,30 @@ var mw = require('./lib/middlewares');
 var co = require('co');
 
 const router = new mw.router();
-
 var app = new koa();
+
 //app.use(mw.router());
 app.use(mw.bodyParser());
+
+router.get('/', (ctx) => {
+  ctx.body = "hello world";
+})
 
 router.post('/users', async (next) => {
   next.body = "hi user";
   console.log(next.request.body);
 });
 
-router.get('/', function *() {
-  this.body = "hello world";
-})
-
 app.use(router.routes());
 
-app.use(function *(){
+/*app.use(function *(){
   var url = this.request.url;
   this.body = "hello world";
-});
+});*/
 
 co(function *(){
   var connection = null;
 })
 
-console.log("server online");
 app.listen(3000);
+console.log("server online at port 3000");
