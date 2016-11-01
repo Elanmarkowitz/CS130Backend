5a3f7e5d6ce79acad1c5490a2246f64539b025b3
Elanmarkowitz <elanmarkowitz@ucla.edu>
2016-11-01 02:47:46 -0700

    Async needs to be implemented

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ router.post('/users', (ctx) => {
     console.log(ctx.request.body);
     ctx.body = "/users"
     console.log(pf.users.createUser(ctx.request.body));
-    
+
 })
 router.get('/users/all', (ctx) => {
     console.log("getting users");
@@ -23,29 +23,33 @@ router.get('/users/all', (ctx) => {
         function(x){
             if(x){
                 ctx.response.body = x;
-                console.log(x);
-                ctx.status = 200;
+                //console.log(x);
+                //ctx.status = 200;
             } else {
-                console.log("none found");
-                ctx.status = 400;
+                //console.log("none found");
+                //ctx.status = 400;
             }
         }
     );
 })
-router.get('/users/:id', (ctx) => {
-    console.log("getting user: " + ctx.params.id);
-    pf.users.getUser(ctx.params.id).then(
-        function(x){
-            if (x){
-                ctx.response.body = x.dataValues;
-                console.log(x.dataValues);
-                ctx.status = 200;
-            } else {
-                console.log("user not found");
-                ctx.status = 400
-            }
-        }
-    );
+router.get('/users/:id', function *() {
+    var ctx = this;
+    console.log("generating");
+    var user = yield pf.users.getUser(ctx.params.id);
+    if(user) ctx.response.status = 200;
+    // then(
+    //     function(x){
+    //         if (x){
+    //             ctx.response.status = 200;
+    //             ctx.response.body = x.dataValues;
+    //             console.log(x.dataValues);
+    //         } else {
+    //             ctx.response.status = 400
+    //             console.log("user not found");
+    //         }
+    //     }
+    // );
+    //console.log("getting user: " + ctx.params.id);
 })
 
 //app.use(mw.bodyParser());
