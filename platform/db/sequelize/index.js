4232f0b93320d13e95ea14642869af0c3722f3eb
Elanmarkowitz <elanmarkowitz@ucla.edu>
2016-11-04 11:20:13 -0700

    added support for cleardb mysql on heroku

--- platform/db/sequelize/index.js
+++ platform/db/sequelize/index.js
@@ -1,13 +1,27 @@
 var fs = require('fs');
 var path = require('path');
 var Sequelize = require('sequelize');
-var username = "rethrift";
-var password = "rethriftpassword";
-var options = {
-    dialect: "mysql",
-    port:    3306
-};
-var client = new Sequelize('rethrift-db', username, password, options);
+var heroku_deploy = true;
+if (heroku_deploy){
+    var name = 'heroku_303e07628ba128d';
+    var username = 'b174a5579cb408';
+    var password = 'a71e760d';
+    var options = {
+        host: 'us-cdbr-iron-east-04.cleardb.net'
+        dialect: "mysql",
+        port:    3306
+    }
+} else {
+    var name = 'rethrift-db';
+    var username = "rethrift";
+    var password = "rethriftpassword";
+    var options = {
+        host: 'localhost'
+        dialect: "mysql",
+        port:    3306
+    }
+}
+var client = new Sequelize(host, username, password, options);
 var models = {};
 
 // read all models and import them into the "db" object
